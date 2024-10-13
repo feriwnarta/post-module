2bc2312352468e811b96c2ba0e4a6e39c5b25a56
feriwinarta <84898884+feriwnarta@users.noreply.github.com>
2024-10-13 20:56:33 +0700

    Update MenuProvider.php

--- app/Providers/MenuProvider.php
+++ app/Providers/MenuProvider.php
@@ -17,7 +17,7 @@ class MenuProvider extends ServiceProvider
                     <div class="card-body">
                         <h5 class="card-title">Warehouse Module</h5>
                         <p class="card-text">Warehouse Module</p>
-                        <a href="/invoice/" class="btn btn-primary">Go somewhere</a>
+                        <a href="/post/" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             HTML;
